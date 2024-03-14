cf35ec444e52b08f91744abf67efd36c2b2d4cc9
Alec <alectorres0@gmail.com>
2024-03-13 20:37:44 -0700

    each page calls the categories items

--- src/components/Jewelery.jsx
+++ src/components/Jewelery.jsx
@@ -1,8 +1,28 @@
-
+import {useState, useEffect} from 'react'
+import { getJewelery } from '../API'
 
 const Jewelery = () =>{
+    const [jewelery, setJewelery] = useState([])
+    useEffect(()=>{
+        const getData = async () =>{
+            const data = await getJewelery();
+            setJewelery(data);
+        }
 
-    return (<h1>Jewelery</h1>)
+        getData();
+
+    },[])
+    
+    return (
+    <div>
+    <h1>Electronics</h1>
+        {jewelery.map((item)=>{
+            return(<p key = {item.id}>{`${item.title}`}</p>)
+
+        })}
+    
+    </div>
+    )
 }
 
 export default Jewelery
