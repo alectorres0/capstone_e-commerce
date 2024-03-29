b1159df2dd7c5e59227fa876e649ebc6863b366b
Alec <alectorres0@gmail.com>
2024-03-28 19:03:48 -0700

    commit

---
+++ server/db.js
@@ -0,0 +1,23 @@
+const pg = require('pg');
+const client = new pg.Client('postgres://localhost/The_Best_Store');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+client
+
+}
