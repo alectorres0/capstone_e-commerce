920937deef5ccd76d46cc9aa344eda2d6affca35
Alec <alectorres0@gmail.com>
2024-04-03 13:12:14 -0700

    created multiple different functions

--- server/db.js
+++ server/db.js
@@ -20,6 +20,7 @@ const bcrypt = require('bcrypt');
 
 const createTables = async() =>{
 const SQL = `
+DROP TABLE IF EXISTS cart;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
 id UUID PRIMARY KEY,
@@ -32,9 +33,13 @@ city VARCHAR(255) NOT NULL,
 street VARCHAR(255) NOT NULL,
 zipcode VARCHAR(255) NOT NULL,
 phone VARCHAR(255) NOT NULL
-)
-
-`
+);
+CREATE TABLE cart(
+id UUID PRIMARY KEY,
+user_id UUID REFERENCES users(id) NOT NULL,
+products JSON 
+);
+`;
 await client.query(SQL);
 };
 
@@ -42,12 +47,71 @@ const createUser = async({email, username, password, firstname, lastname, city, 
 const SQL = `
 INSERT INTO users(id, email, username, password, firstname, lastname, city, street, zipcode, phone)
 VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+RETURNING *
 `;
 const response = await client.query(SQL, [uuid.v4(), email, username, await bcrypt.hash(password,5),firstname,lastname,city,street,zipcode,phone]);
+//console.log("user" + JSON.stringify(response.rows[0]));
 return response.rows[0];
 
 }
 
+const authenticate = async({ username})=> {
+    const SQL = `
+      SELECT id, username FROM users WHERE username=$1;
+    `;
+    const response = await client.query(SQL, [username]);
+    if(!response.rows.length){
+      const error = Error('not authorized');
+      error.status = 401;
+      throw error;
+    }
+    return { token: response.rows[0].id };
+  };
+const createCart = async({userid}) =>{
+    const SQL = `
+    INSERT INTO cart(id, user_id)
+    VALUES($1, $2)
+    RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(),userid]);
+    //console.log("cart" + JSON.stringify(response.rows[0]));
+    return response.rows[0];
+}
+const addToCart = async({cartid, products}) =>{
+  const fetchedProducts = await fetchProducts({cartid: cartid});
+  console.log(fetchedProducts);
+  if (!fetchedProducts) {
+    const SQL = `
+    UPDATE cart 
+    SET products = $1
+    WHERE id = $2
+    RETURNING *
+    `;
+    const response = await client.query(SQL, [JSON.stringify(products),cartid]);
+    //console.log("added items to cart: " + JSON.stringify(response.rows[0]))
+    return response.rows[0]
+  } else {
+  fetchedProducts.push(products);
+  const SQL = `
+  UPDATE cart
+  SET products = $1
+  WHERE id = $2
+   RETURNING *
+   `;
+  const response = await client.query(SQL,[JSON.stringify(fetchedProducts),cartid]);
+  //console.log("added item to cart: " + JSON.stringify(response.rows[0]));
+  return response.rows[0];
+   }
+}
+const fetchProducts = async({cartid}) =>{
+const SQL = `
+SELECT products FROM cart where id = $1
+`;
+const response = await client.query(SQL,[cartid]);
+const data = response.rows[0];
+console.log("Fetched Products: " + JSON.stringify(data.products));
+return data.products;
+}
 
 
 
@@ -63,6 +127,10 @@ return response.rows[0];
 module.exports = {
 client,
 createTables,
-createUser
+createUser,
+createCart,
+authenticate,
+fetchProducts,
+addToCart
 
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const {client,createTables,createUser} = require('./db');
+const {client,createTables,createUser,createCart,authenticate,fetchProducts, addToCart} = require('./db');
 
 const express = require('express');
 const app = express();
@@ -14,8 +14,19 @@ console.log('connected to database');
 await createTables();
 console.log('tables created');
 
-await createUser({email: 'julie@email.com', username: 'julie', password:'password', firstname: 'julie', lastname: 'torres', city:'losangeles', street: '4th', zipcode: '675893', phone:'909909909'});
+const julie =  await createUser({email: 'julie@email.com', username: 'julie', password:'password', firstname: 'julie', lastname: 'torres', city:'losangeles', street: '4th', zipcode: '675893', phone:'909909909'});
 console.log('tables seeded')
+console.log("julie ID: " + julie.id)
+
+
+const cart = await createCart({userid: julie.id});
+console.log("cart created");
+await addToCart({cartid: cart.id, products:[{product_id: 4, quantity:3},{product_id:3, quantity:2}]})
+console.log("first time adding to cart ^")
+await addToCart({cartid: cart.id, products:{product_id: 69, quantity:1}})
+console.log("second time");
+await fetchProducts({cartid: cart.id});
+console.log("products fetched");
 app.listen(port, ()=>console.log(`listening on port ${port}`));
 
 }
