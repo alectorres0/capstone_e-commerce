09b4a8e043dc5559a7a877a37a8180ca2128b51d
Alec <alectorres0@gmail.com>
2024-04-08 15:11:09 -0700

    made account symbol and page with userInfo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,18 +11,22 @@ import Register from "./components/Register"
 import Login from "./components/Login"
 import Cart from "./components/Cart"
 import CartItems from './components/CartItems'
+import AccountSymbol from './components/AccountSymbol'
+import Account from './components/Account'
 import {Routes, Route} from "react-router-dom"
 function App() {
   const [cartId,setCartId] = useState(null);
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
   const [cartQuantity, setCartQuantity] = useState(0);
-
+  const [userInfo, setUserInfo] = useState(null);
 
   return (
     <>
     <Navbar/>
+    <AccountSymbol token = {token}/>
     { cartId && <Cart cartQuantity = {cartQuantity} setCartQuantity={setCartQuantity} userId = {userId} token = {token}/>}
+    
     <Routes>
       <Route path = "*" element = {<Home/>}/>
       <Route path = "/men's clothing" element = {<Mens/>}/>
@@ -30,9 +34,10 @@ function App() {
       <Route path = "/electronics" element = {<Electronics/>}/>
       <Route path = "/jewelery" element = {<Jewelery/>}/>
       <Route path = "/item/:name/:id" element = {<SingleItem token = {token} setToken = {setToken} userId = {userId} cartQuantity = {cartQuantity} setCartQuantity = {setCartQuantity}/>}/>
-      <Route path = "/register" element = {<Register cartId = {cartId} setCartId = {setCartId} token = {token} setToken = {setToken} userId = {userId} setUserId = {setUserId}/>}/>
-      <Route path = "/login" element = {<Login cartId = {cartId} setCartId = {setCartId} token = {token} setToken = {setToken} userId = {userId} setUserId = {setUserId}/>}/>
+      <Route path = "/register" element = {<Register cartId = {cartId} setCartId = {setCartId} token = {token} setToken = {setToken} userId = {userId} setUserId = {setUserId} setUserInfo = {setUserInfo}/>}/>
+      <Route path = "/login" element = {<Login cartId = {cartId} setCartId = {setCartId} token = {token} setToken = {setToken} userId = {userId} setUserId = {setUserId} setUserInfo = {setUserInfo}/>}/>
       <Route path = "/cartitems" element = {<CartItems userId = {userId} token = {token} cartId = {cartId} setCartQuantity = {setCartQuantity}/>}/>
+      <Route path = "/account" element = {<Account userInfo = {userInfo}/>}/>
     </Routes>
     
    </>
